4da853218e02e8bce839bf3674e2b87b79b09b67
Emmanuel Dyan <emmanueldyan@gmail.com>
2019-01-01 18:15:41 +0100

    Try to improve wait for travis

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function start_tests(){
+    GOSS_SLEEP=15
+    GOSS_FILES_STRATEGY=cp
+    dgoss run edyan_graylog_test
+}
+
+
 set -e
 
 GREEN='\033[0;32m'
@@ -10,4 +17,4 @@ docker build -t edyan_graylog_test .
 echo ""
 echo -e "${GREEN}Testing ${NC}"
 cd tests
-dgoss run edyan_graylog_test
+start_tests
